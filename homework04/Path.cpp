73a5a745a6e9be297bd750fec0baf9e2c040f5dd
Nathan <nathan@The-Normandy.local>
2020-03-14 23:22:14 -0400

    Path documentation done

--- homework04/Path.cpp
+++ homework04/Path.cpp
@@ -1,11 +1,10 @@
 /*
-Path.cpp represents directories, shell program searches for executable programs
-Written by: Marcos Hernandez (mah47) and Nathan Meyer
-CS232 - Homework 4
+Path.cpp finds directory paths for programs and commands, and handles them
+Written by: Marcos Hernandez (mah47) and Nathan Meyer (tnm6)
+CS232 - Homework 4, Prof. Victor Norman, Calvin University
 March 14, 2020
+IMPORTANT NOTE: Extension given by Prof. Norman
 */
-
-
 
 #include "Path.h"
 #include <dirent.h>
@@ -13,13 +12,17 @@ March 14, 2020
 #include <cstring>
 using namespace std;
 
+/*
+ * Constructor which gathers PATH for device and parses out each entry
+ */
 Path::Path()
 {
   char *fullPath;
   char *curPath;
 
-  fullPath = getenv("PATH");
+  fullPath = getenv("PATH");  // get PATH for device
 
+  // parse through PATH with delimiter ":"
   curPath = strtok(fullPath, ":");
   while (curPath != NULL)
   {
@@ -28,21 +31,27 @@ Path::Path()
   }
 }
 
+/*
+ * From a given string, searches each path for if program is present
+ * Returns index of directory in which it is found
+ * Else, returns -1
+ */
 int Path::find(const string &program) const
 {
   DIR *dir;
   struct dirent *dirEntry;
   int i, numDirs;
 
-  numDirs = directories.size();
+  numDirs = directories.size(); // get numDirs for loop (to save time)
   for (i = 0; i < numDirs; i++)
   {
-    dir = opendir(directories[i].c_str());
+    dir = opendir(directories[i].c_str());    // open each directory
 
-    while ((dirEntry = readdir(dir)) != NULL)
+    while ((dirEntry = readdir(dir)) != NULL) // while there are entries
     {
+      // check if the entry matches the program being searched for
       if (program.compare(dirEntry->d_name) == 0)
-        return i;
+        return i;   // return index if found
     }
 
     closedir(dir);
@@ -51,6 +60,9 @@ int Path::find(const string &program) const
   return -1;
 }
 
+/*
+ * Returns directory of given index i, if valid
+ */
 string Path::getDirectory(int i) const
 {
   if (i < directories.size())
--- homework04/Path.h
+++ homework04/Path.h
@@ -1,3 +1,11 @@
+/*
+Path.h finds directory paths for programs and commands, and handles them
+Written by: Marcos Hernandez (mah47) and Nathan Meyer (tnm6)
+CS232 - Homework 4, Prof. Victor Norman, Calvin University
+March 14, 2020
+IMPORTANT NOTE: Extension given by Prof. Norman
+*/
+
 #ifndef PATH_H
 #define PATH_H
 
