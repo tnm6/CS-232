ae54492580eafc73f5bc1f672f431e98e789e78d
Nathan <nathan@The-Normandy.local>
2020-03-14 23:22:53 -0400

    main.cpp doc done (PROGRAM IS FINISHED)

--- homework04/main.cpp
+++ homework04/main.cpp
@@ -1,8 +1,9 @@
 /*
-main.cpp runs shell program
-Written by: Marcos Hernandez (mah47) and Nathan Meyer
-CS232 - Homework 4
+main.cpp is a simple driver which runs the shell program MNShell
+Written by: Marcos Hernandez (mah47) and Nathan Meyer (tnm6)
+CS232 - Homework 4, Prof. Victor Norman, Calvin University
 March 14, 2020
+IMPORTANT NOTE: Extension given by Prof. Norman
 */
 
 #include "MNShell.h"
