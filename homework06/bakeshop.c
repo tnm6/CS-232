21ecc6c793e741d42cef8fbbc0514039fb1d99bb
Nathan <nathan@The-Normandy.local>
2020-04-11 14:01:46 -0400

    More initial stuff (moving to remote)

--- homework06/bakeshop.c
+++ homework06/bakeshop.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <semaphore.h>
 #include <pthread.h>
 
+int MAX_QUEUE_DEPTH = 5;
+
+sem_t cust_queue;
+
 int main(int argc, char** argv) {
-  
+
+  sem_init(&cust_queue, 0, MAX_QUEUE_DEPTH);
+  printf("Bakery starting up...\n");
+
+  return 0;
 }
